8035faa2a50cbb72bf7965631d8d18f0c9015330
danielsdeleo <dan@opscode.com>
2013-04-09 13:44:57 -0700

    Add index creation/update and existing gem skips

--- lib/gem_fetcher/chunk_importer.rb
+++ lib/gem_fetcher/chunk_importer.rb
@@ -11,6 +11,9 @@ module GemFetcher
     def initialize(gems)
       @gems = gems
       @pool = Pool.new(10)
+
+      @latest_specs_by_gem = nil
+      @new_gems = nil
     end
 
     def config
@@ -29,7 +32,42 @@ module GemFetcher
       @quick_marshal_dir ||= File.join(base_dir, "quick/Marshal.4.8")
     end
 
-    def stage
+    def new_gems
+      if @new_gems.nil?
+        @new_gems = []
+        gems.each do |gem|
+          if released_specs_index.include?(gem.gem_info_tuple)
+            log "skipping gem #{gem.name} #{gem.version} (#{gem.platform})"
+          else
+            @new_gems << gem
+          end
+        end
+        @new_gems.reverse!
+      end
+      @new_gems
+    end
+
+    def import
+      if new_gems.empty?
+        log "no new gems in chunk"
+        return
+      end
+
+      stage_gems
+      import_staged_gems
+      update_spec_indexes
+
+      # require 'pp'
+      # pp :released => released_specs_index
+      # pp :prerelease => prerelease_specs_index
+      # pp :latest => latest_specs_index
+      log "Imported #{new_gems.size} gems"
+      log "Release gems: #{released_specs_index.size}"
+      log "Prerelease:   #{prerelease_specs_index.size}"
+      log "Latest gems:  #{latest_specs_index.size}"
+    end
+
+    def stage_gems
       gems.each do |gem|
         pool.job do
           gem.stage_gem
@@ -39,16 +77,21 @@ module GemFetcher
       pool.run_til_done
     end
 
-    def import
-      gems.each do |gem|
+    def import_staged_gems
+      new_gems.each do |gem|
         gem.import_gem
         gem.import_quick_marshal
         add_to_specs_index(gem)
       end
-      # require 'pp'
-      # pp :released => released_specs_index
-      # pp :prerelease => prerelease_specs_index
-      # pp :latest => latest_specs_index
+    end
+
+    def update_spec_indexes
+      write_spec_index_update("specs.4.8", released_specs_index)
+      write_spec_index_update("prerelease_specs.4.8", prerelease_specs_index)
+      write_spec_index_update("latest_specs.4.8", latest_specs_index)
+      mv_spec_index_update("specs.4.8")
+      mv_spec_index_update("prerelease_specs.4.8")
+      mv_spec_index_update("latest_specs.4.8")
     end
 
     def add_to_specs_index(gem)
@@ -66,11 +109,11 @@ module GemFetcher
           # are different, so tack this one on to the list
           current_latest_version[:info_tuples] << gem.gem_info_tuple
         when 1 # gems' version is newer thant in current_latest_version
-          latest_specs_by_gem[gem.name] = {:version => Gem::Version.new(gem.version), :info_tuples => [gem.gem_info_tuple]}
+          latest_specs_by_gem[gem.name] = {:version => gem.version, :info_tuples => [gem.gem_info_tuple]}
         end
         # check if newer, replace
       else
-        latest_specs_by_gem[gem.name] = {:version => Gem::Version.new(gem.version), :info_tuples => [gem.gem_info_tuple]}
+        latest_specs_by_gem[gem.name] = {:version => gem.version, :info_tuples => [gem.gem_info_tuple]}
       end
     end
 
@@ -84,23 +127,67 @@ module GemFetcher
       @all_gems ||= Set.new(Dir["#{base_dir}/*/*/*gem"].map {|g| File.basename(g) })
     end
 
-    # TODO: initially read from disk
     def released_specs_index
-      @released_specs_index ||= []
+      @released_specs_index ||= read_spec_index("specs.4.8")
     end
 
     def latest_specs_index
       latest_specs_by_gem.values.inject([]) {|index, i| index.concat(i[:info_tuples])}
     end
 
-    # TODO: initially read from disk
     def prerelease_specs_index
-      @prerelease_specs_index ||= []
+      @prerelease_specs_index ||= read_spec_index("prerelease_specs.4.8")
     end
 
-    # TODO: read from disk initially
     def latest_specs_by_gem
-      @latest_specs_by_gem ||= {}
+      if @latest_specs_by_gem.nil?
+        @latest_specs_by_gem = {}
+        spec_index = read_spec_index("latest_specs.4.8")
+        spec_index.each do |name, version, platform|
+          @latest_specs_by_gem[name] ||= {:version => version, :info_tuples => []}
+          @latest_specs_by_gem[name][:info_tuples] << [name, version, platform]
+        end
+      end
+      @latest_specs_by_gem
+    end
+
+    def write_spec_index_update(basename, data)
+      path = File.join(base_dir, "#{basename}.gz.tmp")
+      log "staging spec index '#{basename}' to #{path}"
+      gzip(path, Marshal.dump(data))
+    end
+
+    def mv_spec_index_update(basename)
+      tmp_path = File.join(base_dir, "#{basename}.gz.tmp")
+      path = File.join(base_dir, "#{basename}.gz")
+      log "installing spec index #{basename} at #{tmp_path} to #{path}"
+      FileUtils.mv(tmp_path, path)
+    end
+
+    def read_spec_index(basename)
+      path = File.join(base_dir, "#{basename}.gz")
+      if File.exist?(path)
+        log "loading spec index #{basename} from #{path}"
+        Marshal.load(gunzip(path))
+      else
+        []
+      end
+    end
+
+    def gunzip(file)
+      File.open(file, "r") do |data|
+        Zlib::GzipReader.new(data).read
+      end
+    end
+
+    def gzip(filename, data)
+      Zlib::GzipWriter.open("#{filename}") do |io|
+        io.write(data)
+      end
+    end
+
+    def log(string)
+      $stdout.print("#{string}\n")
     end
 
     # def update_specs_index(index, source, dest)
--- lib/gem_fetcher/controller.rb
+++ lib/gem_fetcher/controller.rb
@@ -12,21 +12,28 @@ module GemFetcher
     def initialize(config)
       @config = config
       @remote_gems = RemoteSource.new(config)
+      @chunk_index = 0
     end
 
     def chunk_size
       100
     end
 
+    def available_gems
+      @available_gems ||= remote_gems.list
+    end
+
     def next_chunk
-      # TODO: remove list of gems we already have
-      remote_gems.list[0...chunk_size]
+      chunk = available_gems[@chunk_index, chunk_size]
+      @chunk_index += chunk_size
+      chunk
     end
 
     def run
-      importer = ChunkImporter.new(next_chunk)
-      importer.stage
-      importer.import
+      until (chunk = next_chunk).empty?
+        importer = ChunkImporter.new(chunk)
+        importer.import
+      end
     end
 
   end
