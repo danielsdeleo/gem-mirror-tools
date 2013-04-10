a32725ea4d0a8816088e68342011f90ea995042b
danielsdeleo <dan@opscode.com>
2013-04-10 16:24:09 -0700

    mo gems mo kinds of corruption

--- lib/gem_fetcher/gem_mover.rb
+++ lib/gem_fetcher/gem_mover.rb
@@ -45,7 +45,7 @@ module GemFetcher
       else
         blacklist!("Gem #{gem_base_name} could not be fetched from upstream")
       end
-    rescue Psych::SyntaxError, Gem::Package::FormatError => e
+    rescue Psych::SyntaxError, Gem::Package::FormatError, Gem::Package::TarInvalidError => e
       blacklist!(e)
     rescue ArgumentError => e
       # ArgumentError is pretty generic :(
